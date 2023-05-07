62e17c3be4fd2cedcdab2c68d916c8461fbb06a6
Karsten Schmidt <k@thi.ng>
2023-05-08 00:11:31 +0200

    feat: update/extend font editor

--- src/font-edit.js
+++ src/font-edit.js
@@ -9,6 +9,8 @@ const gridX = 108;
 const gridY = 3;
 const viewX = 3;
 const viewY = 3;
+const sampleX = viewX;
+const sampleY = viewY + 72;
 let sel = 65;
 let sampleID = 0;
 let mx, my;
@@ -26,11 +28,12 @@ function charGrid() {
 
 function charView() {
 	let addr = FONT_BASE + sel * 9;
-	let w = max(0, peek(addr++) - 1) * 8 + 4;
+	const w = peek(addr++);
+	let markerX = viewX + max(0, w - 1) * 8 + 4;
 	for (let y = 0; y < 8; y++) {
 		let row = peek(addr + y);
 		for (let x = 0; x < 8; x++) {
-			if (bitSet(row, 7 - x)) {
+			if (bitTest(row, 7 - x)) {
 				rect(viewX + x * 8, viewY + y * 8, 8, 8, 12, 1);
 			} else {
 				setpixel(viewX + x * 8 + 4, viewY + y * 8 + 4, 14);
@@ -38,10 +41,11 @@ function charView() {
 		}
 	}
 	if (cx >= 0) rect(viewX + cx * 8, viewY + cy * 8, 8, 8, 3);
-	setpixel(viewX + w, viewY + 66, 2);
-	hline(viewX + w - 1, viewY + 67, 3, 2);
-	hline(viewX + w - 2, viewY + 68, 5, 2);
-	text(`#${sel}`, viewX + 66, viewY, 12, 0);
+	setpixel(markerX, viewY + 66, 2);
+	hline(markerX - 1, viewY + 67, 3, 2);
+	hline(markerX - 2, viewY + 68, 5, 2);
+	text(`ID:\nW:`, viewX + 66, viewY, 12, 0);
+	text(`${sel}\n${w}`, viewX + 78, viewY, 12, 0);
 }
 
 function tick() {
@@ -49,6 +53,7 @@ function tick() {
 	mx = peek(MOUSEX);
 	my = peek(MOUSEY);
 	bt = peek(MOUSE_BUTTONS);
+	pbt = peek(PMOUSE_BUTTONS);
 	if (bt & 1) {
 		if (hitm(gridX, gridY, 128, 128)) {
 			sel = ((my - gridY) >> 3) * 16 + ((mx - gridX) >> 3);
@@ -57,6 +62,10 @@ function tick() {
 			poke(addr, peek(addr) | (1 << (7 - ((mx - viewX) >> 3))));
 		} else if (hitm(viewX, viewY + 66, 64, 8)) {
 			poke(FONT_BASE + sel * 9, 1 + ((mx - viewX) >> 3));
+		} else if (!(pbt & 1) && hitm(sampleX, sampleY, 8, 8)) {
+			sampleID = max(0, sampleID - 1);
+		} else if (!(pbt & 1) && hitm(sampleX + 56, sampleY, 8, 8)) {
+			sampleID = min(samples.length - 1, sampleID + 1);
 		}
 	}
 	if (hitm(viewX, viewY, 64, 64)) {
@@ -69,19 +78,25 @@ function tick() {
 	} else {
 		cx = cy = -1;
 	}
-	for (let i = 1; i <= samples.length; i++) {
-		if (keyp(i)) {
-			sampleID = i - 1;
-			break;
-		}
-	}
 	if (keyp("Backspace") || keyp("Delete")) {
 		memset(FONT_BASE + sel * 9 + 1, 8, 0);
+	} else {
+		const ascii = allKeysA();
+		if (ascii.length) sel = ascii[0];
 	}
 	rect(viewX, viewY, 64, 64, 14);
 	rect(gridX, gridY, 128, 128, 14);
 	rect(gridX + (sel % 16) * 8, gridY + (sel >> 4) * 8, 8, 8, 0, 1);
 	charGrid();
 	charView();
-	text(samples[sampleID], viewX, viewY + 80, 12, 0);
+	tile1(5, sampleX, sampleY, 1);
+	tile1(6, sampleX + 56, sampleY, 1);
+	text("SAMPLE", sampleX + 13, sampleY, 12, 0);
+	text(samples[sampleID], sampleX, sampleY + 10, 12, 0);
+	tile1(1, 0, HEIGHT - 8);
+
+	tiles([2, 3, 3, 4], viewX + 66, viewY + 56, 1);
+	text("SAVE", viewX + 69, viewY + 56, 12, 15);
+
+	tile1(0, mx - 1, my - 1, 1);
 }

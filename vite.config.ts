dfa85b2b64d1b02a4882eea5b05358239518fc6b
Karsten Schmidt <k@thi.ng>
2024-10-27 21:14:07 +0100

    build: update deps & vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-import { defineConfig } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
-
-export default defineConfig({
-	server: {
-		// https://developer.chrome.com/articles/x-google-ignore-list/
-		// https://vitejs.dev/config/server-options.html#server-sourcemapignorelist
-		sourcemapIgnoreList: (path) =>
-			path.includes("node_modules") && !path.includes("@thi.ng"),
+export default {
+	build: {
+		target: "esnext",
+		sourcemap: true,
 	},
-	plugins: [createHtmlPlugin({ minify: true })],
-});
+};
